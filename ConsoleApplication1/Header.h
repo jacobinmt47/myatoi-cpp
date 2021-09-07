36487d5dea5936efbdd83d0c115ff82562a85821
jacob l <jacobinmt47@outlook.com>
2021-09-06 19:45:14 -0600

    Revert "delete if around line 110"
    This reverts commit 3d462db865e71cda741d268ec89efe76996f8b24.

--- ConsoleApplication1/Header.h
+++ ConsoleApplication1/Header.h
@@ -108,7 +108,13 @@ public:;
                 arr.push_back(s[i]);
                 nextChar++;
             }
+            else {
+                if (s[i] == ' ') {
+                    // need to convert to numbers and sum
+                    break;
+                }
                 break;
+            }
         }
         string ret = arr;
         long n = ret.length() - 1;
